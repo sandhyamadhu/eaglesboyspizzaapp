375262b627f9b5bc9a51795dd4af1fec3aa602c1
sandhyamadhu <sandhya@appcare.co.in>
2017-10-11 14:59:07 +0530

    initial commit

--- app/src/main/java/com/example/appcare/eaglesboys/utils/HttpHandler.java
+++ app/src/main/java/com/example/appcare/eaglesboys/utils/HttpHandler.java
@@ -1,15 +1,9 @@
 package com.example.appcare.eaglesboys.utils;
 
-import android.content.Context;
-
-import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
-import com.android.volley.RequestQueue;
 import com.android.volley.Response;
-import com.android.volley.RetryPolicy;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 
